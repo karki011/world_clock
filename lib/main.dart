a0198471831024fb2328c06d0ca680d128609651
Binup karki <karki011@gmail.com>
2020-04-20 11:51:25 -0500

    added safe area and basic text showing in the screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,4 @@
 import 'package:flutter/material.dart';
+import 'package:worldclock/pages/home.dart';
 
 void main() => runApp(MaterialApp(home: Home()));
-
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
