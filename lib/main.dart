036aeb837214ecc9e661fc061bb459770933af6a
Binup karki <karki011@gmail.com>
2020-04-20 13:03:38 -0500

    setup route

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:worldclock/pages/choose_location.dart';
 import 'package:worldclock/pages/home.dart';
+import 'package:worldclock/pages/loading.dart';
 
-void main() => runApp(MaterialApp(home: Home()));
+void main() => runApp(MaterialApp(
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    }));
